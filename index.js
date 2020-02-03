c291ba9e1b64681f49d8d42bed439f9e44445110
António Meireles <antonio.meireles@reformi.st>
2020-02-03 00:11:00 +0000

    stop consuming 'make-promises-safe'
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

--- index.js
+++ index.js
@@ -2,13 +2,6 @@
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
 require('coffeescript/register')
-// Node.js would crashes if there is an uncaught exception, while it does not crash
-// if there is an 'unhandledRejection', i.e. a Promise without a .catch() handler.
-// so, to catch misbehaving we make sure that there are no (hidden) surprises...
-// make-promises-safe installs an process.on('unhandledRejection') handler
-// with an exit code of 1, just like any uncaught exception.
-// vd https://github.com/mcollina/make-promises-safe
-require('make-promises-safe')
 // does what it says ...
 packageJson = require('./package.json')
 Vieramatic = require('./lib/homebridge')
