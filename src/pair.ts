1173390b83d1fde2bd001c3980eefca0fa6e37c8
António Meireles <antonio.meireles@reformi.st>
2021-04-02 20:34:03 +0100

    turn pair.ts a litle less barroque
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

--- src/pair.ts
+++ src/pair.ts
@@ -1,19 +1,15 @@
 #!/usr/bin/env node
 import { VieraTV } from './viera'
 
+const ip = process.argv.slice(2)
 const oops = (e: Error): void => {
   console.error(`${e.message} \n`)
   process.exit(-1)
 }
 
-try {
-  const ip = process.argv.slice(2)
-  if (ip.length !== 1)
-    throw Error('Please specify your Panasonic TV IP address as the (only) argument')
+if (ip.length !== 1)
+  oops(Error('Please specify your Panasonic TV IP address as the (only) argument'))
 
-  VieraTV.setup(ip[0])
-    .then(() => process.exit(0))
-    .catch(oops)
-} catch (e) {
-  oops(e)
-}
+VieraTV.setup(ip[0])
+  .then(() => process.exit(0))
+  .catch(oops)
