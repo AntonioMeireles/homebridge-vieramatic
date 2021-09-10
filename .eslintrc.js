8841db84f1a3f1152d7e85bb698b3edd89bbe10a
António Meireles <antonio.meireles@reformi.st>
2021-09-10 09:34:02 +0100

    sanitize eslintrc
    it's functionally exactly the same as before, it's just that some
    redundant directives were removed.
    
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
 
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
 
     'plugin:json/recommended',
@@ -12,29 +11,61 @@ module.exports = {
     'plugin:import/warnings',
     'plugin:import/typescript',
 
-    'standard',
     'standard-with-typescript',
 
-    'plugin:prettier/recommended',
-    'prettier'
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist'],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 2018, project: './tsconfig.json', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 2018,
+    project: './tsconfig.json',
+    sourceType: 'module'
+  },
   plugins: ['prettier', '@typescript-eslint', 'import', 'json', 'promise', 'eslint-comments'],
   rules: {
-    '@typescript-eslint/no-misused-promises': ['error', { checksVoidReturn: false }],
-    '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: false
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '^_'
+      }
+    ],
     'eslint-comments/no-unused-disable': 'error',
     'import/exports-last': 'error',
     'import/group-exports': 'error',
     'import/order': [
       'error',
-      { alphabetize: { caseInsensitive: true, order: 'asc' }, 'newlines-between': 'always' }
+      {
+        alphabetize: {
+          caseInsensitive: true,
+          order: 'asc'
+        },
+        'newlines-between': 'always'
+      }
     ],
     'json/*': ['error', 'allowComments'],
-    'sort-keys': ['error', 'asc', { caseSensitive: false, natural: true }],
-    'sort-vars': ['error', { ignoreCase: true }]
+    'sort-keys': [
+      'error',
+      'asc',
+      {
+        caseSensitive: false,
+        natural: true
+      }
+    ],
+    'sort-vars': [
+      'error',
+      {
+        ignoreCase: true
+      }
+    ]
   },
-  settings: { 'import/core-modules': ['homebridge'] }
+  settings: {
+    'import/core-modules': ['homebridge']
+  }
 }
