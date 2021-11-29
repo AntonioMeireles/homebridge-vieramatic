302f989183397d8e6abf936456b6a6844c64667b
António Meireles <antonio.meireles@reformi.st>
2021-11-29 19:33:09 +0000

    .prettierrc -> .prettierrc.js
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'none',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'auto',
+  arrowParens: 'always',
+  bracketSpacing: true
+}
