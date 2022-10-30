e91e607fa4ab3a29af3a579e0ec16eac73977b98
António Meireles <antonio.meireles@reformi.st>
2022-10-30 12:28:32 +0000

    update ts-jest and fix jest.config.cjs to supress deprecation warnings
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,11 +1,14 @@
 module.exports = {
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-      useESM: true
-    }
-  },
   preset: 'ts-jest/presets/default-esm',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        isolatedModules: true,
+        useESM: true
+      }
+    ]
+  },
   verbose: true
 }
